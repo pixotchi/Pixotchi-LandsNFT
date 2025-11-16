27b1bacc373132fcc5a9b4ff2dbeb0e29d39676e
Pixotchi <153137364+pixotchi@users.noreply.github.com>
2025-11-16 15:47:39 +0100

    Update LibPayment.sol

--- src/libs/LibPayment.sol
+++ src/libs/LibPayment.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.21;
 
 import "../shared/Structs.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "lib_fork/openzeppelin-v5/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "lib_fork/openzeppelin-v5/token/ERC20/IERC20.sol";
 import {IERC20Burnable} from "../interfaces/IERC20Burnable.sol";
 
 import "./LibConstants.sol";
